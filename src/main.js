5b86edb96220d3aaaf8523a9697391f6749f3d19
bing.wan <1078827038@qq.com>
2020-04-07 16:11:44 +0800

    添加vant&&及首页

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+
+import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+
+
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
+import './styles/index.scss'  // global css
+
 import App from './App'
 import router from './router'
+
+Vue.use(Vant);
 
 Vue.config.productionTip = false
 
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,11 +1,37 @@
 <template>
   <div class='index'>
-    入口
+    <van-nav-bar
+      title="标题"
+      left-text="返回"
+      right-text="按钮"
+      left-arrow
+      @click-left="onClickLeft"
+      @click-right="onClickRight"
+    />
   </div>
 </template>
 
 <script scoped>
 export default {
-  name:'index'
+  name:'index',
+
+  methods:{
+    onClickLeft(){
+      //
+    },
+    onClickRight(){
+      //
+    }
+  }
 }
 </script>
+
+
+<style lang="scss" scoped>
+  .index{
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+</style>
